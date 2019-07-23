048c8a4a863037b64f38cb8339c73b554cd97165
priyankar94 <53145432+priyankar94@users.noreply.github.com>
2019-07-23 08:57:13 +0530

    Add files via upload

---
+++ Sql1-users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `users` (
+`user_id` int(11) NOT NULL,
+`user_name` varchar(255) NOT NULL,
+`user_email` varchar(255) NOT NULL,
+`user_password` varchar(128) NOT NULL,
+`user_status` varchar(1) NOT NULL DEFAULT 'P',
+`user_data` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `users`
+ADD PRIMARY KEY (`user_id`),
+ADD UNIQUE KEY `user_email` (`user_email`),
+ADD KEY `user_name` (`user_name`),
+ADD KEY `user_status` (`user_status`);
+
+ALTER TABLE `users` CHANGE `user_id` `user_id` INT(11) NOT NULL AUTO_INCREMENT;
